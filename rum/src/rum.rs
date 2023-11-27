15e68e14f3525e6eea748db55a1e766f14b2921e
samuelcalise <113198869+samuelcalise@users.noreply.github.com>
2023-11-27 22:34:13 +0000

    cargo check completed

--- rum/src/rum.rs
+++ rum/src/rum.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::process;
+use std::convert::TryInto;
+mod rum;
+mod segments;
+mod registers;
+mod instructions;
+
+//function take from past lab
+pub fn load_instruction(input: Option<&str>) -> Vec<u32> 
+{
+    let mut raw_reader: Box<dyn std::io::BufRead> = match input {
+        None => Box::new(std::io::BufReader::new(std::io::stdin())),
+        Some(filename) => Box::new(std::io::BufReader::new(
+            std::fs::File::open(filename).unwrap(),
+        )),
+    };
+
+    let mut buf = Vec::<u8>::new(); 
+    raw_reader.read_to_end(&mut buf).unwrap();
+
+    let instructions: Vec<u32> = buf
+        .chunks_exact(4)
+        .map(|x| u32::from_be_bytes(x.try_into().unwrap())) 
+        .collect();
+    
+    instructions
+}
+
+
+
+fn main() 
+{
+    let command_line: Vec<String> = env::args().collect();
+
+    let command_file = &command_line[1];
+
+    let runtime_instruction = load_instruction(Some(command_file));
+
+    //let mut rum = rum::Rum::new(&runtime_instruction);
+
+    let instruction_count = 0 as usize;
 }
---
+++ rum/target/debug/deps/rum-4e20735177559dd8.d
@@ -0,0 +1,5 @@
+/workspaces/csc411_assignment5/rum/target/debug/deps/librum-4e20735177559dd8.rmeta: src/lib.rs
+
+/workspaces/csc411_assignment5/rum/target/debug/deps/rum-4e20735177559dd8.d: src/lib.rs
+
+src/lib.rs:
