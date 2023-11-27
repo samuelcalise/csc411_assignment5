91b6c9a254d1da49bf536761bc4878edeeda4d4d
samuelcalise <113198869+samuelcalise@users.noreply.github.com>
2023-11-27 21:15:19 +0000

    starting phase one

---
+++ bitpack/target/debug/deps/bitpack-fb3d33a63986a2f7.d
@@ -0,0 +1,6 @@
+/workspaces/csc411_assignment5/bitpack/target/debug/deps/libbitpack-fb3d33a63986a2f7.rmeta: src/lib.rs src/bitpack.rs
+
+/workspaces/csc411_assignment5/bitpack/target/debug/deps/bitpack-fb3d33a63986a2f7.d: src/lib.rs src/bitpack.rs
+
+src/lib.rs:
+src/bitpack.rs:
