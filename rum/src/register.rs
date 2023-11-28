0430a096d3050a2826affb030a7c45e66eb0f0b0
samuelcalise <113198869+samuelcalise@users.noreply.github.com>
2023-11-28 23:18:01 +0000

    commit

---
+++ rum/src/register.rs
@@ -0,0 +1,28 @@
+
+
+pub struct Register {
+    register: Vec<u32>
+}
+
+
+impl Register{
+
+
+    pub fn new() -> Register
+    {
+        Register{
+            register: vec![0; 8];
+        }
+    }
+
+
+    pub fn get_registerValue(&self, register: usize) -> u32
+    {
+        self.register[register]
+    }
+
+    pub fn set_registerValue(&self, register: usize, some_value: u32)
+    {
+        self.register[register] = some_value
+    }
+}
